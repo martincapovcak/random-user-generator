c40272a82d8d16616b7ab78b7eabda1e6ad84c99
martin capovcak <martincapovcak@martins-Mac-Pro.local>
2020-10-01 18:53:55 +0200

    fetching data from api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    // this code runs ONCE when the component loads..
+    //Here im going to fetch a random user from random-user-generator API
+
+    const fetchData = async () => {
+      const data = await fetch("https://randomuser.me/api/");
+
+      if (data.ok) {
+        const json = await data.json();
+        console.log(json);
+      } else {
+        console.log("HTTP-Error: " + data.status);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Random user generator</h1>
